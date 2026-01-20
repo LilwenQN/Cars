8ab378f0e7905d05105f449d82e117fc8f065fd7
LilwenQN <lilwenqn@gmail.com>
2026-01-20 15:55:40 +0000

    update methods

--- src/cars.py
+++ src/cars.py
@@ -3,15 +3,25 @@ class Cars:
     MaxSpeed = 0
     FuelLevel = 0
 
-    def Cars():
+    def __init__(self):
         pass
 
-    def Accelerate(self):
-        self.CurrentSpeed += 1
-        self.FuelLevel -= 1
+    def Accelerate(self, amount):
+        if (amount > self.MaxSpeed-self.CurrentSpeed):
+            amount = self.MaxSpeed-self.CurrentSpeed
+        self.CurrentSpeed += amount
+        self.FuelLevel -= amount
 
-    def Brake(self):
-        self.CurrentSpeed -= 1
+        if (self.FuelLevel <= 0):
+            self.CurrentSpeed = 0
+
+    def Brake(self, amount):
+        if (amount > self.CurrentSpeed):
+            amount = self.CurrentSpeed
+
+        self.CurrentSpeed -= amount
     
-    def Refuel(self):
-        self.FuelLevel += 1
+    def Refuel(self, amount):
+        self.FuelLevel += amount
+        if (self.FuelLevel > 100):
+            self.FuelLevel = 100
--- src/main.py
+++ src/main.py
@@ -2,3 +2,20 @@ from cars import Cars
 
 car1 = Cars()
 car2 = Cars()
+
+car1.FuelLevel = 100
+car1.CurrentSpeed = 30
+car1.MaxSpeed = 80
+car2.FuelLevel = 50
+car2.CurrentSpeed = 60
+car2.MaxSpeed = 80
+
+print(car1.CurrentSpeed, car1.MaxSpeed, car1.FuelLevel)
+print(car2.CurrentSpeed, car2.MaxSpeed, car2.FuelLevel)
+
+car1.Accelerate(1)
+print(car1.CurrentSpeed, car1.MaxSpeed, car1.FuelLevel)
+car1.Brake(1)
+print(car1.CurrentSpeed, car1.MaxSpeed, car1.FuelLevel)
+car1.Refuel(1)
+print(car1.CurrentSpeed, car1.MaxSpeed, car1.FuelLevel)
